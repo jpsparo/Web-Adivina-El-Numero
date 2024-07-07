69752c11ae2bef3bdb5ef2086643c5e5133556b2
Juan Pablo Sparo <jpsparo@gmail.com>
2024-07-07 17:58:27 -0300

    actualiza cambios

--- app.js
+++ app.js
@@ -5,17 +5,24 @@ let parrafo = document.querySelector('p');
 parrafo.innerHTML = "Indica un número del 1 al 10";
 */
 let numeroSecreto = generarNumeroSecreto();
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 function asignarTextoAElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 generarNumeroSecreto();
 asignarTextoAElemento('h1', 'Juego del número secreto');
 asignarTextoAElemento('p', "Indica un número del 1 al 100");
 
-function intentoDeUsuario(){
-    alert("Click")
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
 }
 function generarNumeroSecreto(){
     return Math.floor(Math.random()* 100)+1;
